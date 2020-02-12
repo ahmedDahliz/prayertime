a8fcb4b13ab317f1e3259e0244d918bdde39f058
ahmedDahliz <dahliz.ahmed1@gmail.com>
2020-02-12 14:58:22 +0100

    first commit

---
+++ src/nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class ListNav extends Component {
+  render () {
+    return (
+      <nav>
+        <ul>
+          
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default ListNav
